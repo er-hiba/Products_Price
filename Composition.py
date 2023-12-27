5eeca57ab731d5cd09d6d240b3d41e624d63f030
Hiba ERRAOUI <125480922+er-hiba@users.noreply.github.com>
2023-12-27 17:50:30 +0100

    :)

---
+++ Composition.py
@@ -0,0 +1,28 @@
+import copy
+
+class Composition():
+    def __init__(self, product, quantity):
+        self.__product = copy.copy(product)
+        self.__quantity = quantity
+
+    @property
+    def get_product(self):
+        return self.__product
+
+    
+    def set_product(self, p):
+        self.__product = p
+
+    @property
+    def get_quantity(self):
+        return self.__quantity
+
+    
+    def set_quantity(self, q):
+        self.__quantity = q
+
+    def __str__(self):
+        return f"\n{str(self.get_product)} \n- Quantity: {self.get_quantity}"
+
+    def __eq__(self, other):
+        return self.get_product == other.get_product and self.get_quantity == other.get_quantity
---
+++ Elementary.py
@@ -0,0 +1,13 @@
+from Product import Product
+
+class Elementary(Product):
+    def __init__(self, name, code, purchase_price):
+        super().__init__(name, code)
+        self.__purchase_price = purchase_price
+
+    @property
+    def get_price(self):
+        return self.__purchase_price
+
+    def __str__(self):
+        return f"{self.get_name}, Code: {self.get_code}, Purchase Price: {self.get_price}"
